c9b4cf170814d0b834d3e9f8a71af63435069485
Nelu_17 <chauhannilay888@gmail.com>
2026-05-14 16:06:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import streamlit as st
 
-st.title("Dhurandher 2 - The Revenge")
+st.title("Google Drive Video Viewer")
 
-# Embed Google Drive preview player in an iframe
+# Google Drive file ID
 file_id = "15UwHNLatFJb8n4CUi0ciXM4nFsLMVhRg"
 preview_url = f"https://drive.google.com/file/d/{file_id}/preview"
 
-st.components.v1.iframe(preview_url, width=800, height=600)
+# Responsive iframe embed
+st.markdown(
+    f"""
+    <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
+        <iframe src="{preview_url}" 
+                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" 
+                frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+        </iframe>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
