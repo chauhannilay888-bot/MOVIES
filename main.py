58202b2f25085563eb250d8d8946cae5f852328a
Nelu_17 <chauhannilay888@gmail.com>
2026-05-14 16:14:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ st.title("Google Drive Video Viewer")
 file_id = "15UwHNLatFJb8n4CUi0ciXM4nFsLMVhRg"
 preview_url = f"https://drive.google.com/file/d/{file_id}/preview"
 
-# Full-width responsive iframe
+# Fullscreen horizontal responsive iframe
 st.markdown(
     f"""
     <style>
-    .video-wrapper {{
-        position: relative;
+    .video-fullscreen {{
+        position: fixed;
+        top: 0;
+        left: 0;
         width: 100vw;   /* full viewport width */
         height: 100vh;  /* full viewport height */
         margin: 0;
         padding: 0;
+        z-index: 9999;  /* keep it on top */
     }}
-    .video-wrapper iframe {{
-        position: absolute;
-        top: 0;
-        left: 0;
+    .video-fullscreen iframe {{
         width: 100%;
         height: 100%;
         border: none;
     }}
     </style>
-    <div class="video-wrapper">
+    <div class="video-fullscreen">
         <iframe src="{preview_url}" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
     """,
