fe739495220ba1092eae9a01aca879d8414991f1
Nelu_17 <chauhannilay888@gmail.com>
2026-05-14 16:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,35 @@ import streamlit as st
 
 st.title("Google Drive Video Viewer")
 
-# Google Drive file ID
+# Your Google Drive file ID
 file_id = "15UwHNLatFJb8n4CUi0ciXM4nFsLMVhRg"
 preview_url = f"https://drive.google.com/file/d/{file_id}/preview"
 
-# Responsive iframe embed
+# Full-width responsive iframe
 st.markdown(
     f"""
-    <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
-        <iframe src="{preview_url}" 
-                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" 
-                frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
-        </iframe>
+    <style>
+    .video-container {{
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-bottom: 100%; /* Default square ratio, will stretch */
+    }}
+    .video-container iframe {{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }}
+    @media (max-width: 768px) {{
+        .video-container {{
+            padding-bottom: 75%; /* Adjust for mobile screens */
+        }}
+    }}
+    </style>
+    <div class="video-container">
+        <iframe src="{preview_url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
     """,
     unsafe_allow_html=True
