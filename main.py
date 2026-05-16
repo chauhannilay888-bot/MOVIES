1fb67b8e82a1a9ad9b95223ee7c98d24ceafbea0
Nelu_17 <chauhannilay888@gmail.com>
2026-05-16 22:31:21 +0530

    Enhance video embedding with responsive iframe
    Updated the video embedding to use a responsive iframe with full-screen support and added CSS for better styling.

--- main.py
+++ main.py
@@ -3,8 +3,29 @@ import streamlit as st
 # Google Drive video embed link
 video_url = "https://drive.google.com/file/d/15UwHNLatFJb8n4CUi0ciXM4nFsLMVhRg/preview"
 
-# Use st.components.v1.html to render the iframe
+# Responsive iframe with full-screen support
 st.components.v1.html(
-    f'<iframe src="{video_url}" width="640" height="480" allow="autoplay"></iframe>',
-    height=1080, width=1920
+    f"""
+    <style>
+    .video-container {{
+        position: relative;
+        padding-bottom: 56.25%; /* 16:9 aspect ratio */
+        height: 0;
+        overflow: hidden;
+        max-width: 100%;
+    }}
+    .video-container iframe {{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border: 0;
+    }}
+    </style>
+    <div class="video-container">
+        <iframe src="{video_url}" allow="autoplay; fullscreen"></iframe>
+    </div>
+    """,
+    height=600,
 )
