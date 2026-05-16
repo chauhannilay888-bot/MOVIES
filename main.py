0d44d0b79726b9088eee29e03e899edfc1899084
Nelu_17 <chauhannilay888@gmail.com>
2026-05-16 22:22:21 +0530

    Fix video URL and embed using iframe
    Updated video URL and modified HTML embedding method.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import streamlit as st
-video_url = https://drive.google.com/d/15UwHNLatFJb8n4CUi0ciXM4nFsLMVhRg/preview
-st.v1.components.html(video_url)
+
+# Google Drive video embed link
+video_url = "https://drive.google.com/file/d/15UwHNLatFJb8n4CUi0ciXM4nFsLMVhRg/preview"
+
+# Use st.components.v1.html to render the iframe
+st.components.v1.html(
+    f'<iframe src="{video_url}" width="640" height="480" allow="autoplay"></iframe>',
+    height=1080, width=1920
+)
